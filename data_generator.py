615f54ca75583eeb698831dcdc75b4f9bdda533f
Nathan Greffe <N.Greffe@student.ulg.ac.be>
2019-04-12 09:52:39 +0200

    adding validator

--- data_generator.py
+++ data_generator.py
@@ -4,7 +4,7 @@ from tensorflow.keras.utils import Sequence
 from matplotlib import pyplot as plt
 from albumentations import Compose, RandomSizedCrop, OpticalDistortion, ElasticTransform, RandomRotate90, \
     VerticalFlip, OneOf, GridDistortion, Blur, MotionBlur, GaussNoise, RGBShift, ToGray, RandomBrightnessContrast, \
-    RandomGamma, CLAHE, HueSaturationValue, ChannelShuffle
+    RandomGamma, CLAHE, HueSaturationValue, ChannelShuffle, CenterCrop
 from pathlib import Path
 import numpy as np
 import cv2
@@ -12,7 +12,7 @@ import cv2
 
 class DataGenerator(Sequence):
     def __init__(self, data_directory, output_masks, ids_list=None, batch_size=32,
-                 resolution=128, n_channels_input=3, shuffle=True):
+                 resolution=128, n_channels_input=3, shuffle=True, performs_data_augmentation=True):
         self.data_directory = Path(data_directory)
         self.batch_size = batch_size
         self.n_channels = n_channels_input
@@ -21,22 +21,24 @@ class DataGenerator(Sequence):
         self.output_masks = output_masks
 
         # data augmentation pipeline:
-        small_color_augm = Compose([RandomBrightnessContrast(p=1), RandomGamma(p=1), CLAHE(p=1)], p=.25)
-        medium_color_augm = Compose([CLAHE(p=1), HueSaturationValue(hue_shift_limit=20, sat_shift_limit=50,
-                                                                    val_shift_limit=50, p=1)], p=.25)
-        huge_color_augm = Compose([ChannelShuffle(p=1)], p=.5)  # ChannelShuffle more likely than the 2 others
-        self.augmentations = Compose([RandomSizedCrop(min_max_height=(int(resolution*2/3), int(resolution*3/2)),
-                                                      height=resolution, width=resolution,
-                                                      interpolation=cv2.INTER_NEAREST, p=1.0),
-                                      OneOf([OpticalDistortion(p=1.0, distort_limit=2, shift_limit=0.5),
-                                             GridDistortion(p=.5), ElasticTransform(p=1.0, alpha=120, sigma=120 * 0.05,
-                                                                                    alpha_affine=120 * 0.03)],
-                                            p=.5),
-                                      RandomRotate90(p=.5), VerticalFlip(p=.5),
-                                      OneOf([small_color_augm, medium_color_augm, huge_color_augm], p=.8),
-                                      OneOf([Blur(p=.3), GaussNoise(p=.3), MotionBlur(p=.3)], p=.5),
-                                      OneOf([RGBShift(p=.5), ToGray(p=.5)], p=.3)
-                                      ])  # TODO: add nuclei to cells to improve cell separation, use transforms.Lambda
+        if performs_data_augmentation:
+            small_color_augm = Compose([RandomBrightnessContrast(p=1), RandomGamma(p=1), CLAHE(p=1)], p=.25)
+            medium_color_augm = Compose([CLAHE(p=1), HueSaturationValue(hue_shift_limit=20, sat_shift_limit=50,
+                                                                        val_shift_limit=50, p=1)], p=.25)
+            huge_color_augm = Compose([ChannelShuffle(p=1)], p=.5)  # ChannelShuffle more likely than the 2 others
+            self.preprocessing = Compose([RandomSizedCrop(min_max_height=(int(resolution*2/3), int(resolution*3/2)),
+                                                          height=resolution, width=resolution,
+                                                          interpolation=cv2.INTER_NEAREST, p=1.0),
+                                          OneOf([OpticalDistortion(p=1.0, distort_limit=2, shift_limit=0.5),
+                                                 GridDistortion(p=.5), ElasticTransform(
+                                                  p=1.0, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03)], p=.5),
+                                          RandomRotate90(p=.5), VerticalFlip(p=.5),
+                                          OneOf([small_color_augm, medium_color_augm, huge_color_augm], p=.8),
+                                          OneOf([Blur(p=.3), GaussNoise(p=.3), MotionBlur(p=.3)], p=.5),
+                                          OneOf([RGBShift(p=.5), ToGray(p=.5)], p=.3)
+                                          ])  # TODO: add nuclei to cells to improve cell separation, use transforms.Lambda
+        else:
+            self.preprocessing = Compose([CenterCrop(resolution, resolution, p=1.0)])
 
         if ids_list is None:
             ids_list = [directory.name for directory in self.data_directory.iterdir() if directory.is_dir()]
@@ -59,38 +61,30 @@ class DataGenerator(Sequence):
 
         ids_list_subset = self.ids_list[indexes]
 
-        return self.__data_generation(ids_list_subset, data_augmentation=True)
+        return self.__data_generation(ids_list_subset)
 
     def get_unique_item(self, item):
         """same as item but only gives back a single element"""
         id_item = self.ids_list[self.indexes[item]]
-        return self.__data_generation([id_item], data_augmentation=False)
+        return self.__data_generation([id_item])
 
-    def __data_generation(self, ids_list_subset, data_augmentation):
-        """ take a list of ids of images and return the corresponding batch to feed the network, this step includes data
-        augmentation if data_augmentation is set to True, otherwise the image is rescaled"""
+    def __data_generation(self, ids_list_subset):
+        """ take a list of ids of images and return the corresponding batch to feed the network"""
         # get the images/masks from the files
         images_paths = (self.data_directory / image_id / "images" / (image_id + ".png")
                         for image_id in ids_list_subset)
 
         images = (cv2.imread(str(image_path)) for image_path in images_paths)
-        if not data_augmentation:
-            images = (cv2.resize(image, (self.resolution, self.resolution), interpolation=cv2.INTER_NEAREST)
-                      for image in images)
-        masks = (self.get_channels_masks(id_image=current_id, resize_masks=not data_augmentation)
+        masks = (self.get_channels_masks(id_image=current_id)
                  for current_id in ids_list_subset)
 
         # data augmentation
-        if data_augmentation:
-            processed_images = []
-            processed_masks = []
-            for image, mask in zip(images, masks):
-                augmented = self.augmentations(image=image, mask=mask)
-                processed_images.append(augmented['image'])
-                processed_masks.append(augmented['mask'])
-        else:
-            processed_masks = list(masks)
-            processed_images = list(images)
+        processed_images = []
+        processed_masks = []
+        for image, mask in zip(images, masks):
+            augmented = self.preprocessing(image=image, mask=mask)
+            processed_images.append(augmented['image'])
+            processed_masks.append(augmented['mask'])
 
         # convert to numpy array and rescale
         processed_images = np.array(processed_images)
@@ -100,9 +94,9 @@ class DataGenerator(Sequence):
 
         return processed_images, processed_masks
 
-    def get_channels_masks(self, id_image, resize_masks, processed_dir_name="processed_masks"):
+    def get_channels_masks(self, id_image, processed_dir_name="processed_masks"):
         """ return the masks of an image. The image needs to have been processed and the compiled mask must be in the
-        subdirectory named processed_dir_name, the masks are resized if resize_masks is set to true"""
+        subdirectory named processed_dir_name"""
 
         masks_dir = self.data_directory / id_image / processed_dir_name
 
@@ -111,9 +105,6 @@ class DataGenerator(Sequence):
 
         # read each image as a channel
         mask_channels = (cv2.imread(str(masks_dir / (mask_name + ".png")), 0) for mask_name in self.output_masks)
-        if resize_masks:
-            mask_channels = (cv2.resize(mask_channel, (self.resolution, self.resolution),
-                                        interpolation=cv2.INTER_NEAREST) for mask_channel in mask_channels)
 
         # each channel must be of dimension 2: shape [size1, size2, 1] => [size1, size2]
         mask_channels = (np.squeeze(mask_channel) for mask_channel in mask_channels)
