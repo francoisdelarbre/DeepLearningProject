54690c738a560f59145b78251500fa55e2abd851
NatGr <nathan.2@live.be>
2019-04-23 14:01:00 +0200

    adding arguments for smaller fc-densenets

--- sem_segmentation.py
+++ sem_segmentation.py
@@ -19,30 +19,37 @@ from models.vanilla_unet import unet_model
 parser = argparse.ArgumentParser(description='Computing table')
 parser.add_argument('--save_file', default='model', type=str, help='file name of the model, also used as tensorboard '
                                                                    'dir if different from model')
-parser.add_argument('--num_epochs', default=150, type=int, help='number of epochs')
-parser.add_argument('--input_size', default=128, type=int,
-                    help='width/height of the input, used for networks with variable input size '
-                         '(unet_mobilenet/vanilla_unet)')
 parser.add_argument('--model', default='fc-densenet', type=str, help='neural network to use',
                     choices=['fc-densenet', 'unet_mobilenet', 'vailla_unet'])
+parser.add_argument('--optimizer', default='adam', type=str, choices=['adam', 'rmsprop'], help='optimizer to use')
+parser.add_argument('--num_epochs', default=150, type=int, help='number of epochs')
 parser.add_argument('--nbr_channels', default=3, type=int, help='number of channels')
 parser.add_argument('--main_data_dir', default='data/stage1_train', type=str, help='directory containing the principal '
                                                                                    'dataset')
 parser.add_argument('--sec_data_dir', default='data/extra_data', type=str,
                     help='directory containing another dataset that looks like the main one to improve results, set to '
                          '"" if only the main dataset is to be used')
-parser.add_argument('--sec_data_dir_factor', default=4, type=int,
-                    help='set this value to use more the secondary data set, this makes sense if, for example, the '
-                         'images are bigger in the secondary dataset (you can ignore this if you do not use a secondary'
-                         'dataset ')
 parser.add_argument('--batch_size', default=8, type=int, help='size of a batch')
 parser.add_argument('--train_prop', default=.9, type=float, help='proportion of training set w.r.t. complete dataset')
 parser.add_argument('--out_masks', default='["union_mask", "weight_mask"]', type=str,
                     help='output masks as a json string, weight mask should be the last if it is present')
+
+# parameters that are unrelevant for some choices of the other parameters
+parser.add_argument('--input_size', default=128, type=int,
+                    help='width/height of the input, used for networks with variable input size '
+                         '(unet_mobilenet/vanilla_unet)')
+parser.add_argument('--sec_data_dir_factor', default=4, type=int,
+                    help='set this value to use more the secondary data set, this makes sense if, for example, the '
+                         'images are bigger in the secondary dataset (you can ignore this if you do not use a secondary'
+                         'dataset ')
 parser.add_argument('--non_border_cells_weights', default=30, type=int,
                     help="if 'weight_mask' in output_masks: the weight to give to non_border_cells pixels (border cells"
                          " pixels have a weight of 255)")
-parser.add_argument('--optimizer', default='adam', type=str, choices=['adam', 'rmsprop'], help='optimizer to use')
+parser.add_argument('--fcdensenet_num_channels', default=0, type=int,
+                    help="number of blocks to use by subnetwork, if 0 is given, will use the values for a "
+                         "fc-densenet103 instead, to be ignored if model isn't densenet")
+parser.add_argument('--fcdensenet_growth_rate', default=16, type=int,
+                    help="growth factor to use, to be ignored if model isn't fc-densenet")
 
 args = parser.parse_args()
 
@@ -78,8 +85,11 @@ if __name__ == "__main__":
 
     inputs = Input(shape=(input_size, input_size, args.nbr_channels))
     num_classes = len(out_masks) - (1 if 'weight_mask' in out_masks else 0)
+
     if args.model == 'fc-densenet':
-        output = FCDenseNet().compute_output(inputs, num_classes)
+        fcdensenet = FCDenseNet(growth_rate=args.fcdensenet_growth_rate) if args.fcdensenet_num_channels == 0 else \
+            FCDenseNet(num_channels=[args.fcdensenet_num_channels]*6, growth_rate=args.fcdensenet_growth_rate)
+        output = fcdensenet.compute_output(inputs, num_classes)
     elif args.model == 'unet_mobilenet':
         output = unet_mobilenetv2(inputs, num_classes, shape=(input_size, input_size, args.nbr_channels),
                                   mobilenet_upsampling=True)
@@ -93,7 +103,7 @@ if __name__ == "__main__":
     if args.optimizer == 'adam':
         optimizer = Adam(lr=0.00008)
     elif args.optimizer == 'rmsprop':
-        optimizer = RMSprop(lr=1e-3, decay=0.995)
+        optimizer = RMSprop(lr=1e-4, decay=0.998)
     else:
         raise ValueError("invalid optimizer")
 
